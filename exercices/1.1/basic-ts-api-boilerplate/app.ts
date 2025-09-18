0de8aff0ae39c9a917d2c4dcdef5bcdef8581e63
kim.bryere <kim.bryere@student.vinci.be>
2025-09-18 17:17:02 +0200

    add tout

--- exercices/1.1/basic-ts-api-boilerplate/app.ts
+++ exercices/1.1/basic-ts-api-boilerplate/app.ts
@@ -2,11 +2,21 @@ import express from "express";
 
 import filmsRouter from "./routes/films";
 
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/films", filmsRouter);
+
+let count = 0;
+app.use((req, _res, next) =>{
+    if(req.method === "GET"){
+        count++;
+        console.log('GET counter : ${count}')
+    }
+    next();
+})
+
+
 export default app;
