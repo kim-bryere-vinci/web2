67ba8ebd9dbb6f781ff97e45e99ce3f49401ca74
kim.bryere <kim.bryere@student.vinci.be>
2025-10-13 16:39:37 +0200

    new: ex2.3

---
+++ exercices/2.1-2-3-4/2.1-2-3-4/src/components/Cinema/index.tsx
@@ -0,0 +1,26 @@
+import type { Movie } from "../Movie";
+
+
+interface CinemaProps {
+    name: string;
+    movies: Movie[];
+  }
+
+ 
+const Cinema = (props: CinemaProps) => {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <ul>
+            {props.movies.map((movie) => (
+        <li key={movie.title}>
+            <strong>{movie.title}</strong>- Réalisateur :{" "}
+            {movie.director}
+          </li>
+            ))}
+        </ul>
+      </div>
+    );
+  };
+
+  export default Cinema;
