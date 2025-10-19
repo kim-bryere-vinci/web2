ab1953a9d0a9015eaa09c8bebded9dfb0d138706
kim.bryere <kim.bryere@student.vinci.be>
2025-10-19 20:53:39 +0200

    tuto 2.c

---
+++ tutorials/start-state/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+interface HeaderProps {
+    title: string;
+    version: number;
+  }
+  
+  const Header = ({title, version}: HeaderProps) => {
+    return (
+      <header>
+        <h1 className= "animate__animated animate__bounce">{title}</h1>
+        <h4>Version: {version}</h4>
+      </header>
+    );
+  };
+
+  export default Header;
